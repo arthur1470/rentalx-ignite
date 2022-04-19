ccd5f4ec905cce1595372494fe2813431b998ea3
Arthur Marques <arthur.l2@hotmail.com>
2022-04-18 22:17:20 -0300

    Refatorando estrutura do projeto

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,8 +1,8 @@
 import { inject, injectable } from "tsyringe";
 
-import { AppError } from "@errors/AppError";
 import { ICreateCategoryDTO } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
+import { AppError } from "@shared/errors/AppError";
 
 @injectable()
 class CreateSpecificationUseCase {
