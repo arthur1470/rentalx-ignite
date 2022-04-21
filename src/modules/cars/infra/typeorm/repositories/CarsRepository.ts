9adb4018651408247a9a3ef14d25bac7a226fa8c
Arthur Marques <arthur.l2@hotmail.com>
2022-04-21 18:18:43 -0300

    Listagem de carros disponiveis

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -37,6 +37,23 @@ class CarsRepository implements ICarsRepository {
     async findByLicensePlate(license_plate: string): Promise<Car> {
         return this.repository.findOne({ license_plate });
     }
+
+    async findAvailable(
+        name?: string,
+        brand?: string,
+        category_id?: string,
+    ): Promise<Car[]> {
+        const carsQuery = this.repository
+            .createQueryBuilder("c")
+            .where("available = :available", { available: true });
+
+        if (name) carsQuery.andWhere("c.name = :name", { name });
+        if (brand) carsQuery.andWhere("c.brand = :brand", { brand });
+        if (category_id)
+            carsQuery.andWhere("c.category_id = :category_id", { category_id });
+
+        return carsQuery.getMany();
+    }
 }
 
 export { CarsRepository };
--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -19,6 +19,18 @@ class CarsRepositoryInMemory implements ICarsRepository {
     async findByLicensePlate(license_plate: string): Promise<Car> {
         return this.cars.find(car => car.license_plate === license_plate);
     }
+
+    async findAvailable(
+        name?: string,
+        brand?: string,
+        category_id?: string,
+    ): Promise<Car[]> {
+        return this.cars
+            .filter(car => car.available === true)
+            .filter(car => !name || car.name === name)
+            .filter(car => !brand || car.brand === brand)
+            .filter(car => !category_id || car.category_id === category_id);
+    }
 }
 
 export { CarsRepositoryInMemory };
