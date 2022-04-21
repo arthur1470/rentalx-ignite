dbbb34b25dfd06fbd36290a78968d1c0c9bebe1e
Arthur Marques <arthur.l2@hotmail.com>
2022-04-21 16:34:21 -0300

    Seed pra cadastrar usuario admin

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,17 +3,20 @@ import express, { Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "@shared/infra/typeorm";
-import "@shared/container";
-
 import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
+
+import "@shared/container";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
+createConnection();
+
 const app = express();
 app.use(express.json());
 app.use(router);
+
 app.use((err: Error, _request: Request, response: Response) => {
     if (err instanceof AppError)
         return response.status(err.statusCode).json({ message: err.message });
@@ -23,6 +26,7 @@ app.use((err: Error, _request: Request, response: Response) => {
         message: `Internal server error - ${err.message}`,
     });
 });
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => {
