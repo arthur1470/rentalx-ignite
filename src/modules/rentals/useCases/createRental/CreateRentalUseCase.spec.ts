626699da854d45436a307da65767935be5935683
Arthur Marques <arthur.l2@hotmail.com>
2022-04-23 12:38:53 -0300

    Cadastro de rentals e testes

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,59 @@
+import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
+
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
+
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+let createRentalUseCase: CreateRentalUseCase;
+
+describe("Create Rental", () => {
+    beforeEach(() => {
+        rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+        createRentalUseCase = new CreateRentalUseCase(
+            rentalsRepositoryInMemory,
+        );
+    });
+
+    it("Should be able to create a new rental", async () => {
+        const rental = await createRentalUseCase.execute({
+            user_id: "12345",
+            car_id: "121212",
+            expected_return_date: new Date(),
+        });
+
+        expect(rental).toHaveProperty("id");
+        expect(rental).toHaveProperty("start_date");
+    });
+
+    it("Should not be able to create a new rental if there's another open to the same user", async () => {
+        expect(async () => {
+            await createRentalUseCase.execute({
+                user_id: "123",
+                car_id: "123",
+                expected_return_date: new Date(),
+            });
+
+            await createRentalUseCase.execute({
+                user_id: "123",
+                car_id: "1234",
+                expected_return_date: new Date(),
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+
+    it("Should not be able to create a new rental if there's another open to the same car", async () => {
+        expect(async () => {
+            await createRentalUseCase.execute({
+                user_id: "123",
+                car_id: "123",
+                expected_return_date: new Date(),
+            });
+
+            await createRentalUseCase.execute({
+                user_id: "321",
+                car_id: "123",
+                expected_return_date: new Date(),
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+});
