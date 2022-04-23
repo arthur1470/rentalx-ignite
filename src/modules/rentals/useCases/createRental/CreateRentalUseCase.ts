bca9fd6e4be0f54e9f71510ae229c3d312db3bd5
Arthur Marques <arthur.l2@hotmail.com>
2022-04-23 12:53:46 -0300

    Validando tempo minimo de aluguel

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,6 +1,11 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { AppError } from "@shared/errors/AppError";
+
+dayjs.extend(utc);
 
 interface IRequest {
     user_id: string;
@@ -16,6 +21,8 @@ class CreateRentalUseCase {
         car_id,
         expected_return_date,
     }: IRequest): Promise<Rental> {
+        const minRentalHour = 24;
+
         const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
             car_id,
         );
@@ -27,6 +34,11 @@ class CreateRentalUseCase {
 
         if (rentalOpenToUser)
             throw new AppError("There's a rental in progress for user!");
+
+        const dateNow = dayjs().utc().local().format();
+        const compare = dayjs(expected_return_date).diff(dateNow, "hours");
+
+        if (compare < minRentalHour) throw new AppError("Invalid return time!");
 
         return this.rentalsRepository.create({
             user_id,
