0d5babfd702cd0161f4fe9ad2b56de38728ede46
Arthur Marques <arthur.l2@hotmail.com>
2022-04-24 11:18:51 -0300

    Enviando email de recuperacao de senha

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,9 +1,11 @@
+import { resolve } from "path";
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidv4 } from "uuid";
 
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 import { AppError } from "@shared/errors/AppError";
 
 @injectable()
@@ -15,10 +17,19 @@ class SendForgotPasswordMailUseCase {
         private usersTokensRepository: IUsersTokensRepository,
         @inject("DayjsDateProvider")
         private dateProvider: IDateProvider,
+        @inject("EtherealMailProvider")
+        private mailProvider: IMailProvider,
     ) {}
     async execute(email: string): Promise<void> {
         const user = await this.usersRepository.findByEmail(email);
-
+        const templatePath = resolve(
+            __dirname,
+            "..",
+            "..",
+            "views",
+            "emails",
+            "forgotPassword.hbs",
+        );
         if (!user) throw new AppError("User does not exists!");
 
         const token = uuidv4();
@@ -28,6 +39,18 @@ class SendForgotPasswordMailUseCase {
             user_id: user.id,
             expires_date: this.dateProvider.addHours(3),
         });
+
+        const variables = {
+            name: user.name,
+            link: `${process.env.FORGOT_MAIL_URL}${token}`,
+        };
+
+        await this.mailProvider.sendMail(
+            user.email,
+            "Recuperacao de senha",
+            variables,
+            templatePath,
+        );
     }
 }
 
