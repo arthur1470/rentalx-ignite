2b07e8d231f287a270719ba528cec1102f760a1c
Arthur Marques <arthur.l2@hotmail.com>
2022-04-24 09:38:43 -0300

    UseCase e controller de recuperacao de senha

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -5,6 +5,7 @@ interface IDateProvider {
     compareInHoursFromNow(date: Date): number;
     compareInDays(start_date: Date, end_date: Date): number;
     addDays(days: number): Date;
+    addHours(hours: number): Date;
 }
 
 export { IDateProvider };
